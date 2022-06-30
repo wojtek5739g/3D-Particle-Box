b3fd5a53e29bdb90e59f34b9225ce77f0a167457
wojtek5739g <wojtek5739@gmail.com>
2022-06-30 16:56:51 +0200

    added changing of the size of the box

--- particle.py
+++ particle.py
@@ -9,11 +9,7 @@ from mayavi.core.api import PipelineBase
 from mayavi.core.ui.api import MayaviScene, SceneEditor, \
                 MlabSceneModel
 
-a, b, c = 100, 100, 100
-x = np.arange(a)
-y = np.arange(b)
-z = np.arange(c)
-xx, yy, zz = np.meshgrid(x, y, z)
+h = 6,63*10**(-34)
 
 def cube_faces(xmin, xmax, ymin, ymax, zmin, zmax):
     faces = []
@@ -44,6 +40,9 @@ def cube_faces(xmin, xmax, ymin, ymax, zmin, zmax):
 
     return faces
 
+def Energy(nx, ny, nz):
+    return (h^2/(8*m))*(nx^2/a^2+ny^2/b^2+nz^2/c^2)
+
 def mlab_plt_cube(xmin,xmax,ymin,ymax,zmin,zmax):
     faces = cube_faces(xmin,xmax,ymin,ymax,zmin,zmax)
     for grid in faces:
@@ -55,22 +54,34 @@ class MyModel(HasTraits):
     nx = Range(1, 10, 1)
     ny = Range(1, 10, 1)
     nz = Range(1, 10, 1)
+    a = Range(1, 100, 1)
+    b = Range(1, 100, 1)
+    c = Range(1, 100, 1)
+
     scene = Instance(MlabSceneModel, ())
     plot = Instance(PipelineBase)
     plot2 = Instance(PipelineBase)
 
-    @on_trait_change('nx,ny,nz,scene.activated')
+    @on_trait_change('nx,ny,nz,a,b,c,scene.activated')
     def update_plot(self):
         mlab.clf()
         p, q, r = (self.nx, self.ny, self.nz)
+        a, b, c = (self.a, self.b, self.c)
+
+        x = np.arange(a)
+        y = np.arange(b)
+        z = np.arange(c)
+
+        xx, yy, zz = np.meshgrid(x, y, z)
+
         self.plot = mlab.contour3d(abs((np.sqrt(8/(a*b*c))*np.sin((p*np.pi*xx)/a))*(np.sin((q*np.pi*yy)/b))*(np.sin((r*np.pi*zz)/c)))**2, contours=6, transparent=True, name = 'Particle in 3D box')
         mlab.axes(x_axis_visibility=True, y_axis_visibility=True, z_axis_visibility=True)
-        self.plot2 = mlab_plt_cube(0,a,0,a,0,a)
+        self.plot2 = mlab_plt_cube(0,a,0,b,0,c)
 
     view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene),
                      height=250, width=300, show_label=False),
                 Group(
-                        'nx', 'ny','nz',
+                        'nx', 'ny','nz','a','b','c'
                      ),
                 resizable=True,
                 )
